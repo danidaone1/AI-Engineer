475f311a5e4880d7c2e3ca1efa9c82a53c7f7d23
danidaone1 <daniscode678@gmail.com>
2025-02-12 20:49:55 +0500

    7

---
+++ Python/Variables/Variables_Exercise.py
@@ -0,0 +1,26 @@
+#Question 1: Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+#break=5
+#Answer: The reason break isn't working because its a reserved keyword for another functionality. So python doesn't let use that keyword.
+
+#Question 2: Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+birth_year=2003
+curr_year=2025
+age=curr_year-birth_year
+print("My age would be: ",age)
+#Answer: My Age Would be: 22
+
+#Question 3: Store your first, middle and last name in three different variables and then print your full name using these variables
+first="Daniyal"
+middle="Hassan"
+last="Shahbaz"
+print(first," ",middle," ",last)
+#Answer: Daniyal Hassan Shahbaz
+
+#Question: 4 Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+#Invalid: _nation
+#Invalid: 1reccord
+#Valid: reccord1
+#Valid: reccord_one
+#Invalid: reccord-one
+#Invalid: reccord^one
+#Invalid: continue(reserved keyword) 
