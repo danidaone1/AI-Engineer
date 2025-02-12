95a58eb2a0497955b8aac89ec64e101296979b65
danidaone1 <daniscode678@gmail.com>
2025-02-12 21:49:08 +0500

    8

---
+++ Python/Numbers/Number_Exercise.py
@@ -0,0 +1,15 @@
+#Question 1: You have a football field that is 92 meter long and 48.8 meter wide. Find out total area using python and print it.
+length=92
+width=48.8
+total_area=length*width
+print("Total Area: ",total_area)
+
+#Question 2: You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar and you gave shopkeeper 20 dollar. Find out using python, how many dollars is the shopkeeper going to give you back?]
+print("Packet cost: ", 20-9*1.49)
+
+#Question: 3 You want to replace tiles in your bathroom which is exactly square and 5.5 feet is its length. If tiles cost 500 rs per square feet, how much will be the total cost to replace all tiles. Calculate and print the cost using python (Hint: Use power operator ** to find area of a square)
+print("COst: ", (5.5**2)*500)
+
+#Question: 4  Print binary representation of number 17
+num=17
+print('Binary of number 17 is:',format(num,'b'))
---
+++ Python/Numbers/Numbers.py
@@ -0,0 +1,7 @@
+distance=10
+speed=50
+time=distance/speed
+print("Total Time will be: ",time)
+print("Round Function test: ",round(time,2))
+
+print("Square root of 5 by 4 is: ",5**4)
