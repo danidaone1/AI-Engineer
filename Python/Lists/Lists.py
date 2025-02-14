113a5a0fe7634eeb9673047fd36fd641fa5a1660
danidaone1 <daniscode678@gmail.com>
2025-02-14 20:56:33 +0500

    12

---
+++ Python/Lists/Lists.py
@@ -0,0 +1,18 @@
+list=["a","b","c","d","e"]
+print(list)
+print(list[1])
+list[1]="x"
+print(list)
+print(list[0:3])
+list.append("z")
+print(list)
+list.insert(0,"W")
+print(list)
+
+list=["a","b","c","d","e"]
+list2=[1,2,3,4,5]
+test=list+list2
+print(test)
+print(len(test))
+print(1 in test)
+print(12 in test)
