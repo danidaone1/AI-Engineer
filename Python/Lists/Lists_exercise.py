6f298c4c8975678d65d623ebb2c3838424734543
danidaone1 <daniscode678@gmail.com>
2025-03-04 17:45:31 +0500

    15

--- Python/Lists/Lists_exercise.py
+++ Python/Lists/Lists_exercise.py
@@ -17,3 +17,29 @@ print("Total expense list after june: ",exp)
 # based on this
 exp[3]=exp[3]-200
 print("Updated expense List of april",exp)
+
+#Question 2.0
+heros=['spider man','thor','hulk','iron man','captain america']
+#Question: 1 Length of the list
+print(len(heros))
+
+#Question: 2 Add 'black panther' at the end of this list
+heros.insert(5,'black panther')
+print(heros)
+
+#Question: 3 You realize that you need to add 'black panther' after 'hulk',
+#so remove it from the list first and then add it after 'hulk'
+heros.remove('black panther')
+print(heros)
+
+heros.insert(3,'black panther')
+print(heros)
+
+#Question: Now you don't like thor and hulk because they get angry easily :)
+#   So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
+#  Do that with one line of code.
+heros[1:3]=['Doctor strange']
+print(heros)
+#Question: 5 Sort the list in alphabetical order
+heros.sort()
+print(heros)
