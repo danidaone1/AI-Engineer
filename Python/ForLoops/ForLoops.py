407523f1a64d92f8abf17e300ee813364c64a54f
danidaone1 <daniscode678@gmail.com>
2025-03-05 21:56:16 +0500

    20

---
+++ Python/ForLoops/ForLoops.py
@@ -0,0 +1,40 @@
+"""
+list1=["a",'b','c','d','e',1,2,3,4,5,6,7,8]
+for x in list1:
+ print(x)
+ if x=='e':
+  break
+"""
+"""
+
+for x in range(2,10,3):
+    if x == 8:
+        break
+    print(x)
+else:
+    print("blah blah")
+
+"""
+
+"""
+*
+**
+***
+****
+*****
+
+"""
+
+for i in range(1,6):
+ s=''
+ for j in range(i):
+  s +='*' 
+ print(s)
+ 
+for x in range(6,1):
+ a=''
+ for y in range(x):
+  a+=' *'
+  print(a)
+
+  
---
+++ Python/ForLoops/forloops2.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    for j in range(4-i):
+        print(" ",end="")
+    for k in range(i+1):
+        print("* ",end="")
+    print("")
+
+for x in range(4):
+    for a in range(x+1):
+        print(" ",end="")
+    for y in range(4-x):
+        print("* ",end="")
+    print("")
+
+
+
+
+    
