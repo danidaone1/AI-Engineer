7869eadce1118cb99ad73a61fc81b82d79270c0b
danidaone1 <daniscode678@gmail.com>
2025-03-14 14:02:02 +0500

    21

---
+++ Python/Dictionaries/Dictionaries.py
@@ -0,0 +1,13 @@
+dic={
+    "Name": "Daniyal",
+    "age":22,
+    "city":"Gujranwala"
+}
+print(len(dic))
+
+dic2=dict(name="Mohamid",age=20,skincolor="nigga",color=['red','blue','white'])
+
+print(dic2)
+
+
+
