33ba6378f76e9ac3b3eb420049558089f07c1483
danidaone1 <daniscode678@gmail.com>
2025-05-20 00:21:24 +0500

    23

---
+++ Python/W3School.py
@@ -0,0 +1,28 @@
+a=" Faraz"
+print(a.upper())
+
+print(a.lower())
+#This removes the whitespace
+print(a.strip())
+
+x=a[0:4]
+
+print(x.replace("a","A",1)) 
+x=x.replace("a","A",1)
+x=x+a[4:]
+print(x)
+
+
+b="Daniyal , Hassan"
+print(b.split(","))
+
+age=22
+name= "daniyal hassan"
+s=f"My name is "+name+f" and my age is {age:5f}"
+print(s)
+
+w=f"Faraz mere paise de {10*100}"
+print(w)
+
+i="Faraz is bharwa and a \" bitch\" hehe!"
+print(i)
