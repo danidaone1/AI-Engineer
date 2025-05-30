2c5aaaa6f9b2c4da8841d4f171c1389b9335ce05
danidaone1 <daniscode678@gmail.com>
2025-05-30 10:05:50 +0500

    1

--- Python/Problems/8_DuplicatesRemove.py
+++ Python/Problems/8_DuplicatesRemove.py
@@ -1,2 +1,11 @@
 #8. Remove Duplicates from a List
 #Remove duplicate elements and return a new list.
+mylist=[1,2,3,4,5,3,4,5]
+newlist=[]
+print("Old list",mylist)
+for i in mylist:
+    if i not in newlist:
+        newlist.append(i)
+print(newlist)
+    
+     
